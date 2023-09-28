bd8976c3bd926b1075dd0078e04da6953f248ec4
vikranth-nallapuneni <nallapunenivikranth17@gmail.com>
2023-09-28 01:13:10 -0400

    added separate folders for components,styles and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,12 @@
-import { useState } from 'react';
+//import { useState } from 'react';
+import Signup from './components/signup';
 import './App.css';
 
 function App() {
-  const [name, SetName] = useState('')
-  const [email, SetEmail] = useState('')
-  const [password, SetPassword] = useState('')
-  
-  async function Register(event) {
-    event.preventDefault()
-    const response = await fetch('http://localhost:8080/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password
-        }),
-    })
-    const data = await response.json()
-    console.log(data)
-  } 
+ 
   
   return (
-    <div>
-      <h1>Registration</h1>
-      <form onSubmit={Register}>
-        <input
-          //value={name}
-          onChange={(e) => SetName(e.target.value)}
-          type = "text"
-          placeholder = "Username"
-        />
-        <br />
-        <input
-          //value={email}
-          onChange={(e) => SetEmail(e.target.value)}
-          type = "text"
-          placeholder = "Email"
-        />
-        <br />
-        <input
-          //value={password}
-          onChange={(e) => SetPassword(e.target.value)}
-          type = "text"
-          placeholder = "Password"
-        /> 
-        <input type = "Submit" value = "Register"/>
-      </form>
-    </div>
+    <Signup></Signup>
   )
 }
 export default App;
---
+++ client/src/components/signup.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+function Signup() {
+  const [name, SetName] = useState('')
+  const [email, SetEmail] = useState('')
+  const [password, SetPassword] = useState('')
+  
+  async function Register(event) {
+    event.preventDefault()
+    const response = await fetch('http://localhost:8080/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        }),
+    })
+    const data = await response.json()
+    console.log(data)
+  } 
+  
+  return (
+    <div>
+      <h1>Registration</h1>
+      <form onSubmit={Register}>
+        <input
+          //value={name}
+          onChange={(e) => SetName(e.target.value)}
+          type = "text"
+          placeholder = "Username"
+        />
+        <br />
+        <input
+          //value={email}
+          onChange={(e) => SetEmail(e.target.value)}
+          type = "text"
+          placeholder = "Email"
+        />
+        <br />
+        <input
+          //value={password}
+          onChange={(e) => SetPassword(e.target.value)}
+          type = "text"
+          placeholder = "Password"
+        /> 
+        <input type = "Submit" value = "Register"/>
+      </form>
+    </div>
+  )
+}
+export default Signup;
