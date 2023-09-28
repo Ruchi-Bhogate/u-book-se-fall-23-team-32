5ae07d29c05c13cd801357fa09e72feb7274b0f5
vikranth-nallapuneni <nallapunenivikranth17@gmail.com>
2023-09-28 16:32:48 -0400

    added sample landing page

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Landing() {
+  
+  return (
+    <div >
+      <h1>Landing Page!</h1> 
+      <p> <a href="/Login">Login</a></p>
+    </div>
+  );
+}
+
+export default Landing;
