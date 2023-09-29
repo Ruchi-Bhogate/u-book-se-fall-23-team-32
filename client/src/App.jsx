7dc842bb3f80a30db8256ea8f405db9540a1fe7d
vikranth-nallapuneni <nallapunenivikranth17@gmail.com>
2023-09-29 01:55:19 -0400

    fixed google authenetication error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,7 @@ import { BrowserRouter as Router, Routes,Route  } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/signup';
 import Landing from './components/Landing';
-import Forgot from './components/Forgot';
-import Reset from './components/Reset';
 import Home from './components/Home';
-import Forgot from './components/Forgot';
-import Reset from './components/Reset';
 
 function App() {
   return (
@@ -18,8 +14,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Landing/>} />
-          <Route path="/Forgot" element={<Forgot/>}/>
-          <Route path="/Reset" element={<Reset/>}/>
           <Route path="/home" element={<Home/>} />
         </Routes>
     </Router>
