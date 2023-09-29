dc78b17dbbbb811594a9ad15097b720c096b2746
Weishun He <weishe@iu.edu>
2023-09-29 01:20:19 -0400

    current version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes,Route  } from 'react-router-dom';
 import Login from './components/Login';
-import Signup from './components/Signup';
+import Signup from './components/signup';
 import Landing from './components/Landing';
+import Forgot from './components/Forgot';
+import Reset from './components/Reset';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Landing/>} />
+          <Route path="/Forgot" element={<Forgot/>}/>
+          <Route path="/Reset" element={<Reset/>}/>
         </Routes>
     </Router>
   );
--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const passportSetup = require('./config/passport-setup');
 const authRoutes = require('./routes/auth');
+const user_model = require('./models/user_model');
+const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer');
 require("dotenv").config();
 
 //app
@@ -14,6 +17,7 @@ const app = express();
 app.use(require('express-session')({ secret: 'secretKey', resave: false, saveUninitialized: false }));
 app.use(passportSetup.initialize());
 app.use(passportSetup.session());
+app.use(express.json())
 
 //db
 mongoose.connect(process.env.MONGO_URI,{
@@ -40,3 +44,36 @@ const port  = process.env.PORT || 8080;
 const server = app.listen(port, () =>
   console.log(`Server is running on port ${port}`)
 );
+
+app.post('/Forgot',(req,res)=> {
+  const {email} = req.body;
+  user_model.findOne({email:email})
+  .then (user => {
+    if(!userr) {
+      return res.send({status:"User does not exist"})
+    }
+    const token = jwt.sign({id: user._id}, "jwt_secret_key", {expiresIn: "Id"})
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'weishe',
+        pass: 'Sat96817?'
+      }
+    });
+    
+    var mailOptions = {
+      from: 'weishunhe92@gmail.com',
+      to: 'myfriend@yahoo.com',
+      subject: 'Password Reset',
+      text: 'http://localhost:8080/reset-password/${user._id}/${token}'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        return res.send({Status:"success"})
+      }
+    });
+  })
+})
