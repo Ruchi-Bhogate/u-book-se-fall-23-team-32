a5e2d2cfdbc5d76fa72441d8ad53e4dadf7497dd
Weishun He <weishe@iu.edu>
2023-09-29 21:37:28 -0400

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,12 @@ import { BrowserRouter as Router, Routes,Route  } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/signup';
 import Landing from './components/Landing';
+<<<<<<< HEAD
 import Home from './components/Home';
+=======
+import Forgot from './components/Forgot';
+import Reset from './components/Reset';
+>>>>>>> dc78b17d (current version)
 
 function App() {
   return (
@@ -14,7 +19,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Landing/>} />
+<<<<<<< HEAD
           <Route path="/home" element={<Home/>} />
+=======
+          <Route path="/Forgot" element={<Forgot/>}/>
+          <Route path="/Reset" element={<Reset/>}/>
+>>>>>>> dc78b17d (current version)
         </Routes>
     </Router>
   );
