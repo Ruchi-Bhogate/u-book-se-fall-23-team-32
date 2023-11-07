f630a9cf9bc8b444b537a0fd4a4c85e662b1b340
Weishun He <weishe@iu.edu>
2023-11-07 01:34:43 -0500

    checking for error

--- server/routes/userview.js
+++ server/routes/userview.js
@@ -56,15 +56,15 @@ router.post('/postbook', verifyToken, upload.single('image'), async (req, res) =
         bookData.owner_user_id = user._id;
         console.log(bookData);
         const newBook = new Book(bookData);
-        //console.log()
+        //console.log('book no make',newBook);
         await newBook.save();
 
         res.json({ message: 'Book posted successfully' });
     } /*catch (error) {
         console.error('Error posting book:', error);
         res.status(500).json({ message: 'Error posting book' });
-    }
-}*/);
+    }*/
+});
 
 module.exports = router;
 
