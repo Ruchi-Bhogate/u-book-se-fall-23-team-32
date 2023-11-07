fb16dd69ccb006083846875a98a1582eabdce871
vikranth-nallapuneni <nallapunenivikranth17@gmail.com>
2023-11-07 00:17:08 -0500

    added posted books page outline

--- client/src/components/BooksGrid.jsx
+++ client/src/components/BooksGrid.jsx
@@ -1,13 +1,15 @@
-import BookCard from "./BookCard";
+import React from 'react';
+import BookCard from './BookCard';
+import '../styles/BooksGrid.css';
 
 function BooksGrid({ books }) {
-    return (
-      <div className="books-grid">
-        {books.map((book) => (
-          <BookCard key={book._id} book={book} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="books-grid">
+      {books.map((book) => (
+        <BookCard key={book._id} book={book} />
+      ))}
+    </div>
+  );
+}
 
-  export default BooksGrid;
+export default BooksGrid;
