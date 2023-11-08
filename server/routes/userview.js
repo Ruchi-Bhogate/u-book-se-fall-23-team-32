51f29231bed8310206a2d0b6bd9041bfaf763552
Weishun He <weishe@iu.edu>
2023-11-07 23:38:00 -0500

    fixing multer

--- server/routes/userview.js
+++ server/routes/userview.js
@@ -27,7 +27,7 @@ const verifyToken = (req, res, next) => {
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, './postedbooks');
+      cb(null, '../postedbooks/');
     },
     filename: function(req, file, cb) {
       cb(null, Date.now() + '-' + file.originalname);
