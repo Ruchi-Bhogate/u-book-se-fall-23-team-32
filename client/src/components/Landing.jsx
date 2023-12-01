885742f021443e2268056da96f9307122cbc4c62
Weishun He <weishe@iu.edu>
2023-12-01 01:57:29 -0500

    landing page change materialui

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import {Typography, CssBaseline, Container, Stack, Box, ButtonGroup, Grid,ThemeProvider} from '@mui/material';
+import {Button} from '@mui/material';
 
-
-function Landing() {
-  
+const Landing = () => {
   return (
-    <div >
-      <h1>Landing Page!</h1> 
-      <p> <a href="/Login">Login</a></p>
-      <p> <a href = "/signup">Signup</a></p>
-    </div>
+    <>
+    <CssBaseline/>
+      
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align="center" color="black" gutterBottom>UBook</Typography>
+            <Typography variant='h5' align="center" color="darksalmon" gutterBottom>Easy Book, Easy Read</Typography>
+           <div>
+                <Box textAlign="center" mt={2}>
+                  <Button style = {{marginRight:"32px"}} variant="contained"href="/Login">Login</Button>
+                  <Button variant="contained"href="/signup">Signup</Button>           
+                </Box>          
+           </div>
+          </Container>
+        </div>
+      </main>
+    </> 
   );
-}
-
+ }
+ 
 export default Landing;
