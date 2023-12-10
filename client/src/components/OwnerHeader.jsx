cca0d54c86cb33b8041908402cdc973e116ecc78
Weishun He <weishe@iu.edu>
2023-12-10 02:27:49 -0500

    made changes to ownerheader

--- client/src/components/OwnerHeader.jsx
+++ client/src/components/OwnerHeader.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import {AppBar, Button, Toolbar, Typography, StepLabel, createTheme, ThemeProvider,} from '@mui/material';
 
-function Header() {
-  return (
-    <header className="header">
-      <div className="logo">Logo</div>
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/ownerdashboard">Dashboard</Link></li>
-          <li><Link to="/postbook">PostBook</Link></li>
-          <li><Link to="/rentedoutbooks">Rented out Books</Link></li>
-          <li><Link to="/ownerprofile">Profile</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-        </ul>
-      </nav>
-    </header>
+const Header = () =>{
+  
+  return ( 
+    <div>
+        <AppBar sx ={{background:'#1c1a1a',opacity:'0.8'}}>
+          <Toolbar>
+            <StepLabel icon={ <img src={require("../images/ubook.png")} alt="" width="110" height="50" /> } />
+            <Button sx={{marginRight: "auto"}} variant="contained" href="/ownerdashboard">Dashboard</Button>
+            <Button sx={{marginRight: "auto"}} variant="contained" href="/postbook">Post Book</Button>
+            <Button sx={{marginRight: "auto"}} variant="contained" href="/rentedoutbooks">Rented Out Books</Button>
+            <Button sx={{marginRight: "auto"}} variant="contained" href="/ownerprofile">OwnerProfile</Button>
+            <Button sx={{marginRight: "auto"}} variant="contained" href="/logout">Logout</Button>
+          </Toolbar>
+        </AppBar>
+    </div>
   );
 }
 
