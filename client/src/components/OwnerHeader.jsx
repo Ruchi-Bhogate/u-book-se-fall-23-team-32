cc2eabca72c5f49758ab676e64ffbe186d9b4411
Isha Sasturkar <ishasasturkar@Ishas-MacBook-Pro.local>
2023-12-10 11:29:21 -0500

    Chat Feature adn some updates for dashboard

--- client/src/components/OwnerHeader.jsx
+++ client/src/components/OwnerHeader.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Header.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+import ChatModal from "./ChatModal";
 
 function Header() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="logo">Logo</div>
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/ownerdashboard">Dashboard</Link></li>
-          <li><Link to="/postbook">PostBook</Link></li>
-          <li><Link to="/rentedoutbooks">Rented out Books</Link></li>
-          <li><Link to="/ownerprofile">Profile</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">Logo</div>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/ownerdashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/postbook">PostBook</Link>
+            </li>
+            <li>
+              <Link to="/rentedoutbooks">Rented out Books</Link>
+            </li>
+            <li>
+              <Link to="/ownerprofile">Profile</Link>
+            </li>
+            <li
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              Chat With Admin
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <ChatModal
+        isModalOpen={isModalOpen}
+        setModalOpen={setModalOpen}
+        withId="6574958e873155beaed7d163"
+        withType="admin"
+        title="Admin"
+      />
+    </>
   );
 }
 
