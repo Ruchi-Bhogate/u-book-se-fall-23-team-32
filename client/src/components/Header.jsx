ccae4771602306fa0c654036c14af51b39d56283
Weishun He <weishe@iu.edu>
2023-12-12 20:48:54 -0500

    final version push

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,12 +10,14 @@ import { Button, AppBar, Toolbar, StepLabel } from "@mui/material";
       return (
       <>
        <div>
-           <AppBar sx ={{background:'#1c1a1a',opacity:'0.8'}}>
+           <AppBar sx ={{background:'#1c1a1a',opacity:'0.8',position: 'relative', marginBottom: '20px', width:"100vw"}}>
              <Toolbar>
                <StepLabel icon={ <img src={require("../images/ubook.png")} alt="" width="110" height="50" /> } />
                <Button sx={{marginRight: "auto"}} variant="contained" href="/dashboard">Dashboard</Button>
                <Button sx={{marginRight: "auto"}} variant="contained" href="/browsebooks">Browse Book</Button>
                <Button sx={{marginRight: "auto"}} variant="contained" href="/profile">User Profile</Button>
+               <Button sx={{marginRight: "auto"}} variant="contained" href="/cartpage">Cart</Button>
+               <Button sx={{marginRight: "auto"}} variant="contained" href="/rentedbookspage">Rented Book</Button>
                <Button sx={{marginRight: "auto"}} variant="contained" href="/logout">Logout</Button>
                <Button sx={{marginRight: "auto"}} variant ="contained" onClick={()=> {setModalOpen(true)}}>Chat with Admin</Button>
              </Toolbar>
